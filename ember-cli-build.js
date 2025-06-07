f81ab96d77c338311a818fe004268a513b9ba9a5
raghulOff <raghulofficework@gmail.com>
2025-06-07 21:08:18 +0530

    Update ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    fingerprint: {
+    prepend: 'https://<your-username>.github.io/<repository-name>/'
+  },
     emberData: {
       deprecations: {
         // New projects can safely leave this deprecation disabled.
